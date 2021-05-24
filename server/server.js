14b2261e86d6d41f40cfb81ebe4b2247ae279ef6
saman <samanf74@gmail.com>
2021-05-24 10:47:00 +0200

    add basic of server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const bodyParser = require("body-parser");
+
+dotenv.config()
+
+const app = express()
+const port = 7474
+let routes = require('./routes.js')
+
+// kill it by ctrl + C
+process.on('SIGINT', function() {
+  console.log("Caught interrupt signal");
+  process.exit(1);
+});
+
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(routes)
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`Example app listening at http://localhost:${port}`)
+  }
+})
