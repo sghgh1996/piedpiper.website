2a7b7b6e5eb5df0b4aff0f2c55ed4c7462b1b852
insaniteaparty <angelo.nasole@mail.polimi.it>
2021-06-23 17:36:23 +0200

    feat[client]: faq pages reworked

--- client/pages/faq.vue
+++ client/pages/faq.vue
@@ -1,39 +1,74 @@
 <template>
   <section class="page">
     <page-banner
-      title="Pied Piper"
+      title="Frequently Asked Questions"
       :img-src="$image('hero.jpg')"
       :breadcrumbs="[]"
     />
 
-    <div class="page-content">
+    <div class="page-content padding-bottom-80">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12">
-            <div class="about">
-              <div class="row">
-                <div class="offset-lg-2 col-lg-8">
-                  <h2>We're makers and creative</h2>
-                  <p>
-                    Pied Piper started as a research spinoff at Politecnico of Milan and was incubated at its startup accelerator. Data and experimentation are part of our DNA. We believe that science and engineering need to be seen as one. It is for this reason that we continuously focus on research, emerging ideas, and new technologies, routinely partnering with top academic and engineering research institutions. Our R&D orientation, combined with data-driven industry and business knowledge, allows us to identify innovation opportunities and create first-to-market solutions, creating unique competitive advantages for our customers.
-                  </p>
-                </div>
-                <div class="col-lg-12">
-                  <div class="about-image">
-                    <div class="img-1">
-                      <img :src="$image('about-1.jpg')" class="img-fluid" alt="pied piper">
-                    </div>
-                    <div class="img-2">
-                      <img :src="$image('about-2.jpg')" class="img-fluid" alt="pied piper">
-                    </div>
+          <div class="col-xs-12">
+            <div class="faq">
+              <div class="card">
+                <div class="card-header">
+                  <div class="mb-0">
+                    <h5 class="faq-title">
+                      <span class="badge">1</span> How does Alya Internet work?
+                    </h5>
                   </div>
                 </div>
-                <div class="offset-lg-2 col-lg-8">
-                  <p>
-                    Telefónica is changing the way users interact with our video products, following the principle of always placing them at the center of the experience. With the ContentWise solution, we can offer viewers personalized and simplified user experiences.
-                  </p>
-                  <h5>Pablo Antolín</h5>
-                  <h6>Product Manager, PayTV - Telefónica</h6>
+                <div class="collapse show">
+                  <div class="card-body">
+                    <p>Donec tempus sodales dolor, at efficitur enim posuere auctor. Nam et nisi eu purus tempor faucibus aliquet vitae orci. Curabitur sollicitudin leo et magna pharetra efficitur. Nullam et scelerisque lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus sit amet odio eget scelerisque. Mauris dictum cursus ornare.</p>
+                    <p>Ut vehicula blandit tellus. Sed sit amet bibendum leo, non sagittis neque. Nam fringilla fermentum tortor, ac gravida velit facilisis id. Donec congue ullamcorper velit, at malesuada arcu faucibus pretium. Donec rhoncus magna sit amet massa venenatis, ut convallis justo ultricies.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header">
+                  <div class="mb-0">
+                    <h5 class="faq-title">
+                      <span class="badge">2</span> How fast is the Alya Internet service?
+                    </h5>
+                  </div>
+                </div>
+                <div class="collapse show">
+                  <div class="card-body">
+                    <p>Donec tempus sodales dolor, at efficitur enim posuere auctor. Nam et nisi eu purus tempor faucibus aliquet vitae orci. Curabitur sollicitudin leo et magna pharetra efficitur. Nullam et scelerisque lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus sit amet odio eget scelerisque. Mauris dictum cursus ornare.</p>
+                    <p>Ut vehicula blandit tellus. Sed sit amet bibendum leo, non sagittis neque. Nam fringilla fermentum tortor, ac gravida velit facilisis id. Donec congue ullamcorper velit, at malesuada arcu faucibus pretium. Donec rhoncus magna sit amet massa venenatis, ut convallis justo ultricies.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header">
+                  <div class="mb-0">
+                    <h5 class="faq-title">
+                      <span class="badge">3</span> Can I keep my home phone number?
+                    </h5>
+                  </div>
+                </div>
+                <div class="collapse show">
+                  <div class="card-body">
+                    <p>Donec tempus sodales dolor, at efficitur enim posuere auctor. Nam et nisi eu purus tempor faucibus aliquet vitae orci. Curabitur sollicitudin leo et magna pharetra efficitur. Nullam et scelerisque lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus sit amet odio eget scelerisque. Mauris dictum cursus ornare.</p>
+                    <p>Ut vehicula blandit tellus. Sed sit amet bibendum leo, non sagittis neque. Nam fringilla fermentum tortor, ac gravida velit facilisis id. Donec congue ullamcorper velit, at malesuada arcu faucibus pretium. Donec rhoncus magna sit amet massa venenatis, ut convallis justo ultricies.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header">
+                  <div class="mb-0">
+                    <h5 class="faq-title">
+                      <span class="badge">4</span> How does the NBN affect my Alya Internet Service?
+                    </h5>
+                  </div>
+                </div>
+                <div class="collapse show">
+                  <div class="card-body">
+                    <p>Donec tempus sodales dolor, at efficitur enim posuere auctor. Nam et nisi eu purus tempor faucibus aliquet vitae orci. Curabitur sollicitudin leo et magna pharetra efficitur. Nullam et scelerisque lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus sit amet odio eget scelerisque. Mauris dictum cursus ornare.</p>
+                    <p>Ut vehicula blandit tellus. Sed sit amet bibendum leo, non sagittis neque. Nam fringilla fermentum tortor, ac gravida velit facilisis id. Donec congue ullamcorper velit, at malesuada arcu faucibus pretium. Donec rhoncus magna sit amet massa venenatis, ut convallis justo ultricies.</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -57,73 +92,76 @@ export default {
 <style lang="scss" scoped>
 .page-content {
 
-  .about {
-    position: relative;
-    float: left;
-    width: 100%;
-    margin-bottom: 100px;
-    text-align: center;
+  .faq {
+    background: #FFFFFF;
+    box-shadow: 0 2px 48px 0 rgba(0, 0, 0, 0.06);
+    border-radius: 4px;
+    margin-bottom: 30px;
 
-    h2 {
-      font-size: 30px;
-      color: #3B566E;
+    .card {
+      border: none;
+      background: none;
+      border-bottom: 1px solid #E2FBFF;
+
+      .card-header {
+        padding: 0px;
+        border: none;
+        background: #E2FBFF;
+
+        .faq-title {
+          width: 100%;
+          text-align: left;
+          padding: 0px;
+          padding-left: 30px;
+          padding-right: 30px;
+          font-weight: 400;
+          font-size: 15px;
+          letter-spacing: 1px;
+          color: #3B566E;
+          text-decoration: none !important;
+          padding-top: 20px;
+          padding-bottom: 20px;
+
+          .badge {
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            line-height: 14px;
+            border-radius: 100px;
+            border-top-left-radius: 5px;
+            border-bottom-left-radius: 5px;
+            text-align: center;
+            background: #169CB1;
+            color: #fff;
+            font-size: 12px;
+            margin-right: 20px;
+          }
+        }
+      }
+
+      .card-body {
+        padding: 30px;
+        padding-left: 35px;
+        padding-bottom: 16px;
+        font-weight: 400;
+        font-size: 16px;
+        color: #6F8BA4;
+        line-height: 28px;
+        letter-spacing: 1px;
+        border-top: 1px solid #F3F8FF;
+
+        p {
+          margin-bottom: 14px;
+        }
+      }
+    }
+
+    @media (max-width: 991px) {
       margin-bottom: 30px;
-    }
 
-    h5 {
-      font-style: italic;
-      font-size: 20px;
-      color: #6F8BA4;
-      letter-spacing: 1.25px;
-      text-align: center;
-      line-height: 28px;
-      margin-top: -20px;
-    }
-
-    .about-image {
-      min-height: 500px;
-      position: relative;
-      margin-top: 50px;
-      margin-bottom: 80px;
-
-      .img-1 {
-        position: absolute;
-        left: 0px;
-        top: 0px;
-        z-index: 2;
-        border-radius: 5px;
-        overflow: hidden;
-        box-shadow: 0 2px 48px 0 rgba(0, 0, 0, 0.13);
-        width: 55%;
-      }
-
-      .img-2 {
-        position: absolute;
-        right: 0px;
-        bottom: 0px;
-        z-index: 1;
-        border-radius: 5px;
-        overflow: hidden;
-        box-shadow: 0 2px 48px 0 rgba(0, 0, 0, 0.13);
-        width: 55%;
-      }
-    }
-  }
-
-  @media (max-width: 991px) {
-    .about {
-
-      .about-image {
-        height: auto;
-
-        .img-1 {
-          position: relative;
-          margin-bottom: 30px;
-        }
-
-        .img-2 {
-          position: relative;
-        }
+      .card .card-header .faq-title {
+        line-height: 26px;
+        margin-top: 10px;
       }
     }
   }
