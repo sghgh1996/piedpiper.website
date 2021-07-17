022599432c6c74099cc352750a3f04e8c9116261
sadjad ghanbary nasab <sghgh1996@gmail.com>
2021-07-17 12:10:39 +0200

    fix:

---
+++ client/pages/areas/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="page">
+    <page-banner
+      title="Pied Piper"
+      :img-src="$image('hero.jpg')"
+      :breadcrumbs="[]"
+    />
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+    getTheArea () {
+      this.$axios.$get('/area/list')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
