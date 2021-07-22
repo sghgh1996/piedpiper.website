e8f0569d640fa576078eb641ba627784ccc75729
saman <samanf74@gmail.com>
2021-07-22 08:18:08 +0200

    channge product model

--- server/models/product.js
+++ server/models/product.js
@@ -4,7 +4,8 @@ const modelName = 'Product';
 
 const ProductSchema = new Schema({
   photo: String,
-  information: String,
+  overview: String,
+  information: [{_id: false, image: String, text: String, title: String}],
   title: String,
   area: { type: Schema.Types.ObjectId, ref: "Area" },
   people: [{ type: Schema.Types.ObjectId, ref: "Person" }],
