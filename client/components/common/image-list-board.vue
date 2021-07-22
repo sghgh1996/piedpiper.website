45f0ab446133c7f7b2f140bfe92567a18f9c334b
saman <samanf74@gmail.com>
2021-07-22 09:15:42 +0200

    now we have image list board component

---
+++ client/components/common/image-list-board.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row main-content">
+            <h2> {{ title }} </h2>
+            <template
+              v-for="(item , index) in items">
+              <div class="col-lg-3 col-md-6 col-sm-6 col-12" :key="index">
+                <image-feature-item
+                  :title="item.title"
+                  :link="`${path_head}/${item._id}`"
+                  :description="item.overview"
+                  :show-arrow="false"
+                  :img-src="item.photo"
+                  :small="true"/>
+              </div>
+            </template>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ImageFeatureItem from './features/image-feature-item'
+
+export default {
+  name: 'image-list-board',
+  components: { ImageFeatureItem },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    path_head: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-content {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  color: #000000;
+
+  h2 {
+    margin-bottom: 40px;
+  }
+
+  p {
+    word-break: break-word;
+  }
+}
+</style>
--- client/pages/areas/_id.vue
+++ client/pages/areas/_id.vue
@@ -8,15 +8,25 @@
     <overview-section
       :text="area.description"
     />
+    <image-list-board
+      title="Suggested Products"
+      :items="area.products"
+      path_head="/products"
+    />
   </section>
 </template>
 
 <script>
 import PageBanner from '../../components/common/page-banner.vue'
 import OverviewSection from '../../components/pages/area/overview-section'
+import ImageListBoard from '../../components/common/image-list-board'
 
 export default {
-  components: { PageBanner, OverviewSection },
+  components: {
+    PageBanner,
+    OverviewSection,
+    ImageListBoard
+  },
   data () {
     return {
       area: {},
@@ -32,6 +42,7 @@ export default {
     this.$axios.get(`/area/${params[2]}`)
       .then((response) => {
         this.area = response.data
+        console.log('=================================')
         console.log(this.area)
         this.breadcrumbs = [
           {
