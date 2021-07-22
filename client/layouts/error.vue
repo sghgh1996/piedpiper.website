25805c0d29f96536893b438b285004d89b3e3e79
saman <samanf74@gmail.com>
2021-07-22 20:32:59 +0200

    adding 404 page

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <div class="colored col-lg-12" style="width: 100%; justify-content: center; display:flex; align-items:center; flex-direction: column; padding: 2em">
+      <img v-if="error.statusCode === 404" :src="$image('404Page.jpeg')" alt="404 Not found">
+      <h1 v-else>An error occurred</h1>
+      <NuxtLink to="/">Return to <b>Home page</b></NuxtLink>
+    </div>
   </div>
 </template>
 
