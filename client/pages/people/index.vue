48e66e5399d6883a7df37004292869e2f4deede1
sadjad <sghgh1996@yahoo.com>
2021-07-24 11:28:55 +0200

    wip: added people filter for role

--- client/pages/people/index.vue
+++ client/pages/people/index.vue
@@ -4,10 +4,26 @@
       :title="title"
       :img-src="$image('hero.jpg')"
       :breadcrumbs="breadcrumbsItems"/>
-
     <div class="container">
       <div class="section">
-        <div class="row">
+        <div class="row mb-5">
+          <select class="career-select">
+            <option
+                value="all"
+                :selected="!roleID"
+              >
+                All
+              </option>
+            <template v-for="career in careers">
+                <option
+                  :value="career._id"
+                  :key="career._id"
+                  :selected="career._id === roleID"
+                >
+                  <span>{{ career.title }}</span>
+                </option>
+            </template>
+          </select>
         </div>
         <div class="row">
           <template v-for="person in people">
@@ -40,6 +56,7 @@ export default {
     return {
       people: [],
       careers: [],
+      roleID: '',
       title: 'All People',
       breadcrumbsItems: [
         {
@@ -50,12 +67,13 @@ export default {
     }
   },
   async mounted () {
+    this.roleID = this.$route.query.role
     this.careers = await this.$axios.$get('/career/list')
-    this.people = await this.$axios.$get('/people/list')
+    if (this.roleID) {
+      this.people = await this.$axios.$get(`/people/career/${this.roleID}`)
+    } else {
+      this.people = await this.$axios.$get('/people/list')
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
